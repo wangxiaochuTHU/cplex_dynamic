7abfb0167ec3c11efe1e7b7873ccc1ebbf5ee189
wangxiaochuTHU <wangxiaochu@tsinghua.org.cn>
2022-09-27 15:47:03 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
 //! This crate is modified based on `rplex`.
+//!
 //! It is a binding of IBM Cplex using its dynamic/shared library.
+//!
+//! Usage: After installing Cplex on your computer, copy `\IBM\ILOG\CPLEX_Studio_Community221\cplex\bin\x64_win64\cplex2210.dll` file into your crate root folder (or its subfolder).
+//!
+//! For example,
+//!
+//! [your-crate]
+//! --[src]
+//! ----main.rs
+//! --[cplex]
+//! ----cplex2210.dll
+//! --Cargo.toml
+//!  
 
 #[macro_use]
 extern crate dlopen_derive;
